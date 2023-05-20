491210e101a3a8b68cde03eea12df4c08d9f8780
Dima Pristupa <dima.pristupa@pm.me>
2023-05-20 14:40:23 +0300

    ♻️ Extract build_size_based_file_index()

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,48 @@
 use std::collections::HashMap;
 use std::env;
+use std::path::PathBuf;
 
 use jwalk::{DirEntry, WalkDirGeneric};
 
 type FileSize = u64;
-type ClientState = ((), Option<FileSize>);
-type ParallelWalkDir = WalkDirGeneric<ClientState>;
+type CustomData = ((), Option<FileSize>);
+type ParallelWalkDir = WalkDirGeneric<CustomData>;
+
+fn build_size_based_file_index(walk_dir_iter: ParallelWalkDir) -> HashMap<FileSize, Vec<PathBuf>> {
+    let mut file_index = HashMap::<FileSize, Vec<PathBuf>>::new();
+
+    for dir_entry_result in walk_dir_iter {
+        let Ok(dir_entry) = dir_entry_result else {
+            unreachable!()
+        };
+
+        let Some(file_size) = dir_entry.client_state else {
+            // NOTE(DP): skip dirs:
+            //           file_size is None only for dirs
+            continue;
+        };
+
+        // let file_name = dir_entry.path().to_string_lossy().into_owned();
+        let file_names = file_index.get_mut(&file_size);
+
+        match file_names {
+            Some(v) => {
+                v.push(dir_entry.path());
+            }
+            None => {
+                let new_file_names = vec![dir_entry.path()];
+                file_index.insert(file_size, new_file_names);
+            }
+        };
+    }
+
+    file_index
+}
 
 fn main() {
     let root_path_for_scan = env::args().nth(1).unwrap_or("./".to_owned());
 
+    // NOTE(DP): iterator preparation
     let walk_dir_iter = ParallelWalkDir::new(&root_path_for_scan)
         .skip_hidden(false)
         .process_read_dir(|_, _, _, dir_entry_results| {
@@ -39,32 +72,7 @@ fn main() {
             });
         });
 
-    let mut file_index = HashMap::<u64, Vec<String>>::new();
-
-    for dir_entry_result in walk_dir_iter {
-        let Ok(dir_entry) = dir_entry_result else {
-            unreachable!()
-        };
-
-        let Some(file_size) = dir_entry.client_state else {
-            // NOTE(DP): skip dirs:
-            //           file_size is None only for dirs
-            continue;
-        };
-
-        let file_name = dir_entry.path().to_string_lossy().into_owned();
-        let file_names = file_index.get_mut(&file_size);
-
-        match file_names {
-            Some(v) => {
-                v.push(file_name);
-            }
-            None => {
-                let new_file_names = vec![file_name];
-                file_index.insert(file_size, new_file_names);
-            }
-        };
-    }
+    let file_index = build_size_based_file_index(walk_dir_iter);
 
     for (file_size, file_names) in file_index
         .into_iter()

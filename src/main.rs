a5cd2f264455e8feca222867fd942badb8f6e990
Dima Pristupa <dima.pristupa@pm.me>
2023-05-20 16:21:22 +0300

    💄 Trivial polishing of application output

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,25 @@ fn main() {
     let root_path_for_scan = env::args().nth(1).unwrap_or("./".to_owned());
     let file_index = FileIndexBuilder::new(root_path_for_scan.as_str()).build();
 
-    for (hash, file_names) in file_index
+    if file_index.is_empty() {
+        println!("No file duplicates");
+        return;
+    }
+
+    println!("Found {} duplicate groups:", file_index.len());
+
+    file_index
         .into_iter()
         .filter(|(_, file_names)| file_names.len() > 1)
-    {
-        println!("hash {hash}: {:?}", file_names);
-    }
+        .for_each(|(hash, file_names)| {
+            println!("Hash {hash}");
+
+            for file_name in file_names {
+                let file_name = file_name.to_string_lossy();
+
+                println!("{file_name}");
+            }
+
+            println!();
+        });
 }

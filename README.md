bb40c6d603ce07e34180063f5811f5307bedd1b6
Dima Pristupa <dima.pristupa@pm.me>
2023-05-23 20:45:17 +0300

    📝 Update help texts

--- README.md
+++ README.md
@@ -4,6 +4,24 @@ Finds duplicate files super fast 🚀
 
 ---
 
-Quick demo:
+### Quick demo:
 
 https://github.com/s373r/storage-wizard/assets/29308899/c4728293-4b44-48d0-93af-bc290b03a841
+
+### Usage
+
+```shell
+Finds duplicate files super fast 
+
+Usage: storage-wizard [OPTIONS] [PATH]
+
+Arguments:
+  [PATH]  Root path for scanning [default: .]
+
+Options:
+  -n, --no-file-index          Do not generate a file index in filesystem
+  -s, --search <FILE_PATH>...  Search for duplicates of specific files
+  -i, --ignore-file-index      Do not use the generated file index to search for duplicates
+  -h, --help                   Print help
+  -V, --version                Print version
+```
--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,22 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(version)]
+#[command(version, about = "Finds duplicate files super fast")]
 pub struct Cli {
-    #[arg(default_value = ".")]
+    #[arg(default_value = ".", help = "Root path for scanning")]
     pub path: String,
 
-    #[arg(short, long)]
+    #[arg(short, long, help = "Do not generate a file index in filesystem")]
     pub no_file_index: bool,
 
-    #[arg(short, long, value_delimiter = ' ', num_args = 1..)]
+    #[arg(short, long, value_delimiter = ' ', num_args = 1.., help = "Search for duplicates of specific files", value_name = "FILE_PATH")]
     pub search: Vec<String>,
 
+    #[arg(
+        short,
+        long,
+        help = "Do not use the generated file index to search for duplicates"
+    )]
     #[arg(short, long, requires = "search")]
     pub ignore_file_index: bool,
 }
